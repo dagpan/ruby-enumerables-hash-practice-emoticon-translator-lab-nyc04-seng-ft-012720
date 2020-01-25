1ca5755c01c1b0c5554361cc90a0ceb69f3891ef
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-25 16:24:51 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,51 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file_path = "./lib/emoticons.yml")
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  result_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  emoticons.each do |key, value|
+
+# the keys inside the 'get_meaning' hash are the Japanese emoticons and point to their meanings
+
+     result_hash["get_meaning"][value[1]] = key
+
+# the keys inside the 'get_emoticon' hash are the English emoticons and point to their meanings
+
+     result_hash["get_emoticon"][value[0]] = value[1]
+  end
+  result_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path = "./lib/emoticons.yml", emo = nil)
   # code goes here
+  hash1 = load_library("./lib/emoticons.yml")
+  if emo
+    if hash1["get_emoticon"].include?(emo)
+       hash1["get_emoticon"].each do |key, value|
+           if key == emo
+              return value
+           end
+       end
+    else
+       return "Sorry, that emoticon was not found"
+    end
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path = "./lib/emoticons.yml", emo = nil)
   # code goes here
+  hash1 = load_library("./lib/emoticons.yml")
+  if emo
+    if hash1["get_meaning"].include?(emo)
+       hash1["get_meaning"].each do |key, value|
+           if key == emo
+              return value
+           end
+       end
+    else
+       return "Sorry, that emoticon was not found"
+    end
+  end
 end
